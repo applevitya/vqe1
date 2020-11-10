bf3cee29f9f6159a5768404f3879d0bdf511a2a8
applevitya <38158336+applevitya@users.noreply.github.com>
2020-11-10 20:43:49 +0300

    new_files

--- examples.py
+++ examples.py
@@ -5,18 +5,25 @@ from qiskit.tools.visualization import circuit_drawer
 from math import *
 
 
-
-qc = QuantumCircuit(2, 2)
+############## Circuit ################
+qc = QuantumCircuit(3,3)
 qc.h(0)
-qc.cx(0, 1)
-qc.measure([0, 1], [0, 1])
+qc.h(1)
+qc.ccx(0,1,2)
+#######################################
 
 
+########### State_vector ##################
+simulation = Aer.get_backend('statevector_simulator')
+print(execute(qc,simulation).result().get_statevector(qc))
+##########################################
+
+# Measuremetns###
+qc.measure(2,2)
+#qc.measure([0, 1], [0, 1])
 backend = BasicAer.get_backend('qasm_simulator')
-job = execute(qc, backend,shots=100)
-plt = plot_histogram(job.result().get_counts(), color='midnightblue', title="New Histogram")
-
+job = execute(qc, backend)
+plt = plot_histogram(job.result().get_counts(qc), color='midnightblue', title="New Histogram")
 qc.draw('mpl').show()
-
-
 plt.show()
+
