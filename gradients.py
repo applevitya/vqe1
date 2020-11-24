524f3f6582738bbf223355d38fea428d2bfd2938
applevitya <38158336+applevitya@users.noreply.github.com>
2020-11-24 20:07:07 +0300

    new_files

--- gradients.py
+++ gradients.py
@@ -2,7 +2,8 @@ from qiskit import *
 from qiskit.visualization import plot_histogram
 from vqe import *
 from qiskit.quantum_info.operators import Operator
-from qiskit.extensions import RXGate, RZGate, RYGate
+from qiskit.extensions import RXGate, RZGate, RYGate, HGate,XGate, IGate, CXGate
+
 
 
 def U(phi, delta):
@@ -19,6 +20,45 @@ def waveplate(phi, delta):
     T = cos(delta / 2) + 1j * sin(delta / 2) * cos(2 * phi)
     R = 1j * sin(delta / 2) * sin(2 * phi)
     return Operator([[T, R], [-R.conjugate(), T.conjugate()]])
+
+
+def derivative_U(phi,delta):
+    Y = Operator(RYGate(2 * phi))
+    Z = Operator(RZGate(delta))
+
+    return 2*Operator(RYGate(2*phi+pi/2)).compose(Z.conjugate(),front = True).compose(Y.transpose(),front =True)- 2*Y.compose(Z.conjugate(),front = True).compose(Operator(RYGate(-2*phi+pi/2)))
+
+
+def U_circuit(phi,N):
+    if N==0:
+        return (((U(phi[0], pi / 2).compose(U(phi[1], pi))).tensor(
+            Operator(XGate).compose(Operator(IGate)))).compose(
+            Operator(CXGate))).compose(
+            (U(phi[2], pi / 2).compose(U(phi[3], pi))).tensor((U(phi[4], pi / 2).compose(U(phi[5], pi)))))
+
+    if N == 1:
+        return (((derivative_U(phi[0],pi/2).compose(U(phi[1],pi))).tensor(Operator(XGate).compose(Operator(IGate)))).compose(Operator(CXGate))).compose((U(phi[2],pi/2).compose(U(phi[3],pi))).tensor((U(phi[4],pi/2).compose(U(phi[5],pi)))))
+    if N==2:
+        return (((U(phi[0], pi / 2).compose(derivative_U(phi[1], pi))).tensor(Operator(XGate).compose(Operator(IGate)))).compose(
+            Operator(CXGate))).compose(
+            (U(phi[2], pi / 2).compose(U(phi[3], pi))).tensor((U(phi[4], pi / 2).compose(U(phi[5], pi)))))
+    if N==3:
+        return (((U(phi[0], pi / 2).compose(U(phi[1], pi))).tensor(Operator(XGate).compose(Operator(IGate)))).compose(
+            Operator(CXGate))).compose(
+            (derivative_U(phi[2], pi / 2).compose(U(phi[3], pi))).tensor((U(phi[4], pi / 2).compose(U(phi[5], pi)))))
+    if N==4:
+        return (((U(phi[0], pi / 2).compose(U(phi[1], pi))).tensor(Operator(XGate).compose(Operator(IGate)))).compose(
+            Operator(CXGate))).compose(
+            (U(phi[2], pi / 2).compose(derivative_U(phi[3], pi))).tensor((U(phi[4], pi / 2).compose(U(phi[5], pi)))))
+    if N==5:
+        return (((U(phi[0], pi / 2).compose(U(phi[1], pi))).tensor(Operator(XGate).compose(Operator(IGate)))).compose(
+            Operator(CXGate))).compose(
+            (U(phi[2], pi / 2).compose(U(phi[3], pi))).tensor((derivative_U(phi[4], pi / 2).compose(U(phi[5], pi)))))
+    if N==6:
+        return (((U(phi[0], pi / 2).compose(U(phi[1], pi))).tensor(Operator(XGate).compose(Operator(IGate)))).compose(
+            Operator(CXGate))).compose(
+            (U(phi[2], pi / 2).compose(U(phi[3], pi))).tensor((U(phi[4], pi / 2).compose(derivative_U(phi[5], pi)))))
+
 
 
 
--- simulations.py
+++ simulations.py
@@ -100,9 +100,12 @@ def optimization():
         log_data(stdout, x, result)
         #log_data(logfile, x, result)
 
+
+    points=[]
     def target_func(x):
+        points.append(H(x)[0])
         return H(x)[0]
-    x0 = [1,1,1,1,2,11]
+    x0 = [1,1,0,1,2,0]
 
     # Gradient for SLSQP#########################
     def gradient_slsqp(x0):
@@ -123,8 +126,8 @@ def optimization():
     #result = minimize_spsa(target_func, callback=callback_func, x0=x0, maxiter=options.iterations,a0=0.01, af=0.01, b0=0.1, bf=0.02)
         #a0=0.05/(0.2*abs(m)+1), af=0.005/(0.2*abs(m)+1), b0=0.1, bf=0.02)
     result = minimize(target_func, x0=x0, method="SLSQP", jac=gradient_slsqp, callback=callback_func, options={'disp':True})
-
-
+    iteration_number = [i for i in range(0,len(points))]
+    #plt.scatter(iteration_number, points, color='r', linestyle='--')
 
 # Order operator
     O = MeanValue(setup, {'hh':(0,0,1,0)})
