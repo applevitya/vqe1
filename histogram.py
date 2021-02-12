a3f627fdd5bcfb79b2bd088ffbae79e418595150
applevitya <38158336+applevitya@users.noreply.github.com>
2021-02-12 21:05:27 +0300

    new_files from macbook pro

--- histogram.py
+++ histogram.py
@@ -4,14 +4,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-with open('Vqe_data/vqe_SLSQP2.txt', 'r') as file:
+with open('Vqe_data/vqe_SLSQP_1grad.txt', 'r') as file:
     nums = [row.strip() for row in file]
 nums = list(map(float, nums))
 
 
-plt.hist(nums,bins=int(len(nums)/20), color = 'blue',density=True)
+plt.hist(nums,bins=int(len(nums)/20), color = 'blue')
 # Plot formatting
-
+plt.xlim(-1.7,-1.3)
 plt.ylabel('')
 plt.xlabel('значение энергии')
 plt.title('Плотность основного состояния гамильтониана Швингера (m = 0)')
