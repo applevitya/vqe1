0c5d01c834dad5d92b2463d5522285758643b493
applevitya <38158336+applevitya@users.noreply.github.com>
2021-03-11 12:26:52 +0300

    add report

--- histogram.py
+++ histogram.py
@@ -4,15 +4,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-with open('Vqe_data/vqe_bad_grad.txt', 'r') as file:
+with open('Vqe_data/vqe_hadamard_slsqp.txt', 'r') as file:
     nums = [row.strip() for row in file]
 nums = list(map(float, nums))
 
 
-plt.hist(nums,bins=int(len(nums)/40), color = 'blue')
+plt.hist(nums,bins=int(len(nums)/100), color = 'blue')
 # Plot formatting
-plt.xlim(-1.6,-1.25)
+#plt.xlim(-1.6,-1.25)
 plt.ylabel('')
 plt.xlabel('значение энергии')
-plt.title('Плотность основного состояния гамильтониана Швингера (m = 0)')
+plt.title('Плотность основного состояния гамильтониана Швингера (m = 0) Hadamard test')
 plt.show()
--- simulations.py
+++ simulations.py
@@ -10,7 +10,7 @@ from math import pi
 from matplotlib import pyplot as plt
 from vqe import *
 from gradients import *
-from spsa import minimize_spsa
+
 from scipy.optimize import minimize
 from examples import energy
 
